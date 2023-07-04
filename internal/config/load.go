67e982fa70c89a7c293e79f42c66e94d024e9c78
mohammadne <mohammadne.dev@gmail.com>
2023-07-04 16:16:33 +0330

    feat: update config

--- internal/config/load.go
+++ internal/config/load.go
@@ -1,21 +1,18 @@
 package config
 
 import (
+	_ "embed"
 	"fmt"
 	"log"
-	"strings"
 
 	"github.com/davecgh/go-spew/spew"
-	"github.com/knadh/koanf/providers/env"
-	"github.com/knadh/koanf/providers/structs"
+	"github.com/knadh/koanf/parsers/yaml"
+	"github.com/knadh/koanf/providers/rawbytes"
 	"github.com/knadh/koanf/v2"
 )
 
 const (
-	delimeter = "."
-	seperator = "__"
-
-	envPrefix = "SNAPPCLOUD_STATUS_BACKEND__"
+	delimiter = "."
 
 	tagName = "koanf"
 
@@ -24,15 +21,11 @@ const (
 )
 
 func Load(print bool) *Config {
-	k := koanf.New(delimeter)
+	k := koanf.New(delimiter)
 
 	// load default configuration from file
-	if err := k.Load(structs.Provider(Default(), "koanf"), nil); err != nil {
-		log.Fatalf("error loading default: %s", err)
-	}
-
-	if err := loadEnv(k); err != nil {
-		log.Printf("error loading environment variables: %v", err)
+	if err := LoadValues(k); err != nil {
+		log.Fatalf("error loading default values: \n%v", err)
 	}
 
 	config := Config{}
@@ -49,15 +42,12 @@ func Load(print bool) *Config {
 	return &config
 }
 
-func loadEnv(k *koanf.Koanf) error {
-	callback := func(source string) string {
-		base := strings.ToLower(strings.TrimPrefix(source, envPrefix))
-		return strings.ReplaceAll(base, seperator, delimeter)
-	}
+//go:embed default.yml
+var values []byte
 
-	// load environment variables
-	if err := k.Load(env.Provider(envPrefix, delimeter, callback), nil); err != nil {
-		return fmt.Errorf("error loading environment variables: %s", err)
+func LoadValues(k *koanf.Koanf) error {
+	if err := k.Load(rawbytes.Provider(values), yaml.Parser()); err != nil {
+		return fmt.Errorf("error loading values: %s", err)
 	}
 
 	return nil
